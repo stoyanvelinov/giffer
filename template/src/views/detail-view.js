15303bd40df0d97cc21508f6851cb8ab093e858b
Veronika Metodieva <veronikamm@abv.bg>
2023-04-12 19:05:22 +0300

    Implement Detailed View

--- template/src/views/detail-view.js
+++ template/src/views/detail-view.js
@@ -1,11 +1,23 @@
+import { EMPTY_HEART } from "../common/constants.js";
+
 export const toGifDetailsView = (gif) => `
+<div class="gif-info">
+      <p>${gif.title}</p>
+    </div>
   <div class="gif-detailed">
+  
     <div class="gif-preview">
       <img src="${gif.images.original.webp}">
     </div>
-    <div class="gif-info">
-      <p>title: ${gif.title}</p>
-      
+    <div class="details">
+      <div id="gif-details">
+      <p> <span><i class="fa fa-calendar"></i></span> ${gif.import_datetime.slice(0, gif.import_datetime.indexOf(' '))}
+      </p>
+      <p> <span><i class="fa fa-user"></i></span> ${gif.username || 'Anonymous'}
+      </p>
+      </div>
+      <span id="heart">${EMPTY_HEART}</span>
+
     </div>
   </div>
 `;
