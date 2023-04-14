85fd19472b7ef91c0eca600ce07d917c8ebe52ad
Veronika Metodieva <veronikamm@abv.bg>
2023-04-14 12:31:28 +0300

    update collection view

--- template/src/views/collection-view.js
+++ template/src/views/collection-view.js
@@ -1,10 +1,10 @@
 import { getGifsByIds } from "../data/api-calls.js";
 import { toSimpleView } from "./simple-view.js";
 export const toCollectionView = async (ids) => {
-  const str = ids.join(",");
-  const arr = await getGifsByIds(str);
+  // const str = ids.join(",");
+  const arr = await getGifsByIds(ids);
   return `
-  <div>
+  <div id="gif-container">
    <h1>My Collection</h1>
    <div id="gif-wrapper">
     <div class="content">
